4f2038988f8996e9f01ceda65114fb7805141c98
noy peled <noypeled27@gmail.com>
2024-08-03 23:43:05 +0300

    add products service

---
+++ FE/src/services/productService.jsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const url = "http://127.0.0.1:8000/api/products";
+
+// get all products from server
+const getAllProducts = async () => {
+  const { data: products } = await axios.get(url);
+  return products;
+};
+
+// get product by id from server
+const getProductById = async (id) => {
+  const protuct = await axios.get(`${url}/${id}`);
+  return protuct;
+};
+
+// sent data of new product to server
+const createProduct = async (newProductData) => {
+  const { data: status } = await axios.post(url, newProductData);
+  return status;
+};
+
+const updateProduct = async (id, obj) => {
+  const { data: status } = await axios.put(`${url}/${id}`, obj);
+  return status;
+};
