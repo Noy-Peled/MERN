b03e7986eeda95b7782df7e51f3b14c2005d6ba3
noy peled <noypeled27@gmail.com>
2024-08-11 22:30:37 +0300

    add customers service

--- BE/services/customersService.js
+++ BE/services/customersService.js
@@ -6,6 +6,7 @@ async function getCustomers() {
   const { data: users } = await axios.get(url);
   const customers = users.map((user) => {
     return {
+      id: user.id,
       name: user.name,
       email: user.email,
       city: user.address.city,
